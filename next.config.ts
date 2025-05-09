84eaffe02d0269d72d337ad8fe86df0314fc50ab
Brian Tam <149264329+BrianTam100@users.noreply.github.com>
2025-05-09 10:58:32 -0400

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export", // Ensures static HTML export
-  basePath: "/my-project", // (Optional) Set this if you are hosting in a subdirectory
-  trailingSlash: true, // (Optional) Adds trailing slashes to URLs
-};
-
-export default nextConfig;
