b4ab769d41e111965ce874c562a91933ebb1ad52
Brian Tam <149264329+BrianTam100@users.noreply.github.com>
2025-05-09 11:59:22 -0400

    update

--- components/MovieDetails.tsx
+++ components/MovieDetails.tsx
@@ -51,7 +51,7 @@ const MovieDetails = ({ category }: MediaType) => {
   const [trailer, setTrailer] = useState<{ results: Trailer[] } | null>(null);
   const { id } = router.query;
   
-  const API_KEY = 'your-api-key';
+  const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZDI0MmE0OTFmZTAzNzc2NzNhODg0YzQ3ODM0NWQzZiIsIm5iZiI6MTc0MzI3MTEwNi44MDcwMDAyLCJzdWIiOiI2N2U4MzRjMmYxZjUzNzY4NzVkZGM5MTEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.U6GroeQplHcTJBZxSZE1D63cRNPZNZDr7ordhOIoSCM';
 
   useEffect(() => {
     if (!id) return;
