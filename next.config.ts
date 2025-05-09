477534933648d985adf19c330fe18d0e0814cce4
Brian Tam <149264329+BrianTam100@users.noreply.github.com>
2025-05-09 09:38:37 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Ensures static HTML export
+  basePath: "/my-project", // (Optional) Set this if you are hosting in a subdirectory
+  trailingSlash: true, // (Optional) Adds trailing slashes to URLs
 };
 
 export default nextConfig;
