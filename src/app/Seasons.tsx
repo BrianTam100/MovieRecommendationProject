75a7f76baeacc2a87b486b6898b78dfa62eb31ae
Brian Tam <149264329+BrianTam100@users.noreply.github.com>
2025-05-10 19:35:19 -0400

    fix seasons

--- src/app/Seasons.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from "react";
-import Image from 'next/image'
-import axios from 'axios';
-
-
-const Seasons = () => {
-    const router = useRouter();
-}
